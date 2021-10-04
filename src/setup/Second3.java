5bd8a335214a1d05b9e88560e36019ebb7d8fe80
Theodor <esincelikyilmaz@gmail.com>
2021-10-04 03:12:14 -0400

    updated second3 file again

--- src/setup/Second3.java
+++ src/setup/Second3.java
@@ -3,15 +3,19 @@ package setup;
 public class Second3 {
 
     public void m(){
+        System.out.println("first method");
 
     }
 
     public void m(int a){
+        System.out.println("second method");
 
     }
 
     public void m(String s){
+        System.out.println("third method");
 
     }
+
 
 }
